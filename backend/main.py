29919942ec3786919c5d9ffae0394131338ef59f
ScarletSun <kacpi.doro@gmail.com>
2025-10-16 16:47:51 +0200

    reworked the parser as a class to allow for easy import into the main file to combine all 3 scripts into a single main file

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from mapper import Mapper
 from scrapper import Scrapper
+from parser import Parser
 
 mapper = Mapper()
 mapper.run(minID=300, maxID=400, output="./output/flows.json")
 
 Scrapper().run(max_workers=5)
+
+Parser().run()
